7d62ee08e1165b1ec7ac5ab7fd634ba04778d6ef
madebyskippy <timothys@nyu.edu>
2017-12-06 11:34:24 -0500

    fancy hover graphics also i noticed a bug

--- Assets/Scripts/TutorialText.cs
+++ Assets/Scripts/TutorialText.cs
@@ -7,11 +7,14 @@ using UnityEngine.UI;
 public class TutorialText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     TutorialUIManager manager;
+	GameObject display;
     [SerializeField] private string tutorialText;
 
     private void Start()
     {
         manager = GameObject.Find("TutorialPanel").GetComponent<TutorialUIManager>();
+		display = transform.GetChild (0).gameObject;
+		display.SetActive(false);
     }
 
     public string GetText()
@@ -23,12 +26,14 @@ public class TutorialText : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     {
         Debug.Log("Show");
 //		GetComponent<Image> ().color = new Color (1f,0.9f,0.36f,0.5f);
+		display.SetActive(true);
         manager.ShowTutorialPanel(this);
     }
 
     public void OnPointerExit(PointerEventData eventData)
 	{
-//		GetComponent<Image> ().color = new Color (1f,0.9f,0.36f,0.0f);
+		//		GetComponent<Image> ().color = new Color (1f,0.9f,0.36f,0.0f);
+		display.SetActive(false);
         manager.ShowTutorialPanel(null);
     }
 }
